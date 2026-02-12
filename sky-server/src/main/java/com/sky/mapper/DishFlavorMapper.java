cd80b4615014230fbd1aa5155ced2509b9557b17
Neptune <471198414@qq.com>
2026-02-12 13:47:21 +0800

    删除菜品业务代码开发

--- sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -4,6 +4,7 @@ import com.sky.annotation.AutoFill;
 import com.sky.entity.DishFlavor;
 import com.sky.enumeration.OperationType;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -16,4 +17,10 @@ public interface DishFlavorMapper {
      */
     @AutoFill(value = OperationType.INSERT)
     void insertBatch(List<DishFlavor> flavors);
+
+    /**
+     * 根据菜品id批量删除口味数据
+     * @param dishIds
+     */
+    void deleteByDishIds(@Param("dishIds") List<Long> dishIds);
 }
