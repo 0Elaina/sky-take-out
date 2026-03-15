077e61d560cba0d0f7fba2ee7ab7292e34dfc04a
Neptune <471198414@qq.com>
2026-03-15 11:31:16 +0800

    管理端查询订单详情业务功能代码开发

--- sky-server/src/main/java/com/sky/controller/admin/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/admin/OrderController.java
@@ -5,9 +5,11 @@ import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.OrderService;
 import com.sky.vo.OrderStatisticsVO;
+import com.sky.vo.OrderVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,9 +37,24 @@ public class OrderController {
         return Result.success(orderService.conditionSearch(ordersPageQueryDTO));
     }
 
+    /**
+     * 订单统计
+     * @return
+     */
     @GetMapping("/statistics")
     public Result<OrderStatisticsVO> statistics() {
         log.info("订单统计");
         return Result.success(orderService.statistics());
+    }
+
+    /**
+     * 查询订单详情
+     * @param id
+     * @return
+     */
+    @GetMapping("/details/{id}")
+    public Result<OrderVO> details(@PathVariable Long id) {
+        log.info("订单详情: {}", id);
+        return Result.success(orderService.details(id));
     }
 }
